9d0e5779d4522bce2151adacb9f75d5c3d0f73d9
Morganxyz <rascaypika@gmail.com>
2023-03-23 18:12:12 +0100

    arrays y objetos en classe avatar

--- src/Anger_Main.java
+++ src/Anger_Main.java
@@ -1,9 +1,25 @@
+import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Anger_Main {
+
+	static Scanner entrada = new Scanner(System.in);
+	static ArrayList <Faccio> llistaFaccions = new ArrayList<>();
+
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		
+	Vermella verm0 = new Vermella("Sangui", "Vermell", "Verd", "Est");
+	Groga grog0 = new Groga("Ketep", "Grog", "Morat", "Nord");
+	Morada mora0 = new Morada("Morau", "Morat", "Grog", "Sud");
+	Verda verd0 = new Verda("Esper", "Verd", "Vermell", "Oest");
+	llistaFaccions.add(verm0);
+	llistaFaccions.add(grog0);
+	llistaFaccions.add(mora0);
+	llistaFaccions.add(verd0);
+	
 	}
 
 }
--- src/Avatar.java
+++ src/Avatar.java
@@ -1,5 +1,5 @@
-import java.util.ArrayList;
 
+import java.util.ArrayList;
 public class Avatar{
 	
 	private String nom;
@@ -12,15 +12,20 @@ public class Avatar{
 	private float resisMag;
 	boolean moviment = false;
 	private int velocitat;
-	Arma arma0;
-	Armadura armad0;
-	Arquero arc0;
-	Warrior war0;
-	Elfs elf0;
-	Enanos ena0;
-	Orcs orc0;
-	Trolls trol0;
+
+	ArrayList<Arma> llistaArmas = new ArrayList<>();
+	Arma arma0 = new Arma("Arma de novato",1,1,1,1,1);
+	ArrayList<Armadura> llistaArmaduras = new ArrayList<>();
+	Armadura armad0 = new Armadura("Armadura de novato",1,1,1,1,1);
+	ArrayList<Classe> llistaClasses = new ArrayList<>();
+	Arquero arc0 = new Arquero("Arquero","Distancia","PetFilia");
+
 	
+		this.war0 = new Warrior("Warrior","Melé","Ira");
+		this.elf0 = new Elfs("Elfo","Herboristeria","Calmado");
+		this.ena0 = new Enanos("Enano","Minería","Fuerte");
+		this.orc0 = new Orcs("Orco","Minería","Rudo");
+		this.trol0 = new Trolls("Troll","Herboristeria","Calmado");
 	
 	
 	public Avatar(String nom, float alsada,float pes, float forsa,float intel,float agil,float resisFis
@@ -34,15 +39,6 @@ public class Avatar{
 		this.agil = agil;
 		this.resisFis = resisFis;
 		this.resisMag = resisMag;
-		this.arma0 = new Arma("Arma de novato",1,1,1,1,1);
-		this.armad0 = new Armadura("Armadura de novato",1,1,1,1,1);
-		this.arc0 = new Arquero("Arquero","Distancia","PetFilia");
-		this.war0 = new Warrior("Warrior","Melé","Ira");
-		this.elf0 = new Elfs("Elfo","Herboristeria","Calmado");
-		this.ena0 = new Enanos("Enano","Minería","Fuerte");
-		this.orc0 = new Orcs("Orco","Minería","Rudo");
-		this.trol0 = new Trolls("Troll","Herboristeria","Calmado");
-
 	
 	}
 
